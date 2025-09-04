085c441ea4902ba719e7b3e1727755fe0301f831
TheSongg <145535169+thesongg@users.noreply.github.com>
2025-09-04 09:45:37 +0800

    增加cookie url

--- core/comm/urls.py
+++ core/comm/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .base_views import BaseViewSet
+from .cookie_views import CookieViewSet
 
 
 router = DefaultRouter()
 router.register(r'', BaseViewSet, basename='')
+router.register(r'cookie', CookieViewSet, basename='cookie')
 
 
 urlpatterns = [
