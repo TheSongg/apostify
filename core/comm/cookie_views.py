72caabd5d21ab586c3b2c634cebacfeca2e4b786
TheSongg <145535169+TheSongg@users.noreply.github.com>
2025-09-09 15:30:11 +0800

    更新 cookie_views.py

--- core/comm/cookie_views.py
+++ core/comm/cookie_views.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 class CookieViewSet(BaseViewSet):
 
     @action(detail=False, methods=['post'])
-    def generate_xhs_cookie(self, request):
+    def generate_xiaohongshu_cookie(self, request):
         nickname = request.data.get('nickname', None)
         generate_xiaohongshu_cookie.delay(nickname)
         return Response("后台执行中~")
