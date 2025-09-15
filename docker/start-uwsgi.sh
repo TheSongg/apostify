5cf55ac958352cd332d3111f1fee45c95182b1f7
TheSongg <145535169+thesongg@users.noreply.github.com>
2025-09-15 20:07:22 +0800

    修改docker-compose.yml

--- docker/start-uwsgi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running Django migrations..."
-python manage.py makemigrations --settings=core.settings
-python manage.py migrate --settings=core.settings
-
-echo "Starting uWSGI..."
-exec uwsgi --ini uwsgi.ini
